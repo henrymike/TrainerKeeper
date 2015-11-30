a326be6c672b99e749d97e13df2b660970a347ab
Mike Henry <chsmike@gmail.com>
2015-11-30 11:33:25 -0500

    Fetching Members and Classes from Parse and inserting into arrays

---
+++ TrainerKeeper/TrainerKeeper/DataManager.swift
@@ -0,0 +1,57 @@
+//
+//  DataManager.swift
+//  Kindling
+//
+//  Created by Mike Henry on 11/4/15.
+//  Copyright © 2015 Mike Henry. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class DataManager: NSObject {
+    
+    //MARK: - Properties
+    static let sharedInstance = DataManager()
+    var membersDataArray = [PFObject]()
+    var classesDataArray = [PFObject]()
+    
+    
+    //MARK: - Fetch Methods
+    
+    func fetchMembersFromParse() {
+        let fetchMembers = PFQuery(className: "Members")
+        fetchMembers.findObjectsInBackgroundWithBlock { (objects, error) -> Void in
+            if error == nil {
+                print("Got Members Data")
+                self.membersDataArray = objects!
+                print("Members Array: \(self.membersDataArray)")
+                dispatch_async(dispatch_get_main_queue()) {
+                    NSNotificationCenter.defaultCenter().postNotification(NSNotification(name: "receivedMembersDataFromServer", object: nil))
+                }
+            } else {
+                print("No Members Data")
+            }
+        }
+        
+    }
+    
+    func fetchClassesFromParse() {
+        let fetchClasses = PFQuery(className: "Classes")
+        fetchClasses.findObjectsInBackgroundWithBlock { (objects, error) -> Void in
+            if error == nil {
+                print("Got Classes Data")
+                self.classesDataArray = objects!
+                print("Classes Array: \(self.classesDataArray)")
+                dispatch_async(dispatch_get_main_queue()) {
+                    NSNotificationCenter.defaultCenter().postNotification(NSNotification(name: "receivedClassesDataFromServer", object: nil))
+                }
+            } else {
+                print("No Classes Data")
+            }
+        }
+        
+    }
+    
+    
+}
--- TrainerKeeper/TrainerKeeper/ViewController.swift
+++ TrainerKeeper/TrainerKeeper/ViewController.swift
@@ -10,9 +10,34 @@ import UIKit
 import Parse
 
 class ViewController: UIViewController {
+    
+    //MARK: - Properties
+    var dataManager = DataManager.sharedInstance
+    
+    
+    
+    //MARK: - Display Methods
+    
+    func newMembersDataReceived() {
+        
+    }
+    
+    func newClassesDataReceived() {
+        
+    }
+    
+    
+    
+    //MARK: - Life Cycle Methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        dataManager.fetchMembersFromParse()
+        dataManager.fetchClassesFromParse()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "newMembersDataReceived", name: "receivedMembersDataFromServer", object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "newClassesDataReceived", name: "receivedClassesDataFromServer", object: nil)
 
     }
 
