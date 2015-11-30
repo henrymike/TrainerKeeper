923d0e3ec62a352f8d64066faa2dd087447aad81
Mike Henry <chsmike@gmail.com>
2015-11-30 10:01:50 -0500

    Initial commit with pod files installed for Parse and ParseUI; verified test data saving successfully to Parse

---
+++ TrainerKeeper/Pods/Target Support Files/Pods-TrainerKeeper/Pods-TrainerKeeper-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TrainerKeeper : NSObject
+@end
+@implementation PodsDummy_Pods_TrainerKeeper
+@end
---
+++ TrainerKeeper/TrainerKeeper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrainerKeeper
+//
+//  Created by Mike Henry on 11/30/15.
+//  Copyright © 2015 Mike Henry. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
